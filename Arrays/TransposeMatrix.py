189c2529646e1dd4f484d678945a1d2aadeac4ee
Khyati Sharma <134150202+khyatishxrma@users.noreply.github.com>
2026-06-20 22:42:03 +0530

    matrix transposition

---
+++ Arrays/TransposeMatrix.py
@@ -0,0 +1,11 @@
+#867 Transpose Matrix
+class Solution:
+    def transpose(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        result = [[0] * m for _ in range(n)]
+
+        for i in range(m):
+            for j in range(n):
+                result[j][i] = matrix[i][j]
+
+        return result
